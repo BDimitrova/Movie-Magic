9cdc16635f2fb5f53b6be2b339e6b60825bdedd5
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-12-12 16:20:21 +0200

    Chain pages

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,19 @@ const router = require('express').Router();
 const homeController = require('./controller/homeController');
 
 router.use(homeController);
+
 router.use('/about', (req, res) => {
     res.render('about');
 });
+
 router.use('/search', (req, res) => {
     res.render('search');
 });
+
+router.use('/create', (req, res) => {
+    res.render('create');
+});
+
 router.use('/*', (req, res) => {
     res.render('404');
 });
